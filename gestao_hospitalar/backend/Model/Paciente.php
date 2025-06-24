e57164d66a766210ea81af591236bcb20bf90321
carllos39 <140573083+carllos39@users.noreply.github.com>
2025-06-24 15:33:51 -0300

    novo

--- gestao_hospitalar/backend/Model/Paciente.php
+++ gestao_hospitalar/backend/Model/Paciente.php
@@ -1,26 +1,26 @@
 <?php
 class Paciente{
-    private ?int $id_paciente;
+    private ?int $id;
     private string $nome;
-    private string $sobreNome;
-    private string $dataDeNascimento;
-    private int $genero;
-    private DateTime $dataDeCadastro;
+    private string $sobrenome;
+    private string $data_nascimento;
+    private string $sexo;
+    private string $data_cadastro;
 
-    public function __construct(?int $id_paciente,$nome,$sobreNome,$dataDeNascimento,$genero,$dataDeCadastro){
+    public function __construct(?int $id,$nome,$sobrenome, $data_nascimento,$sexo,$data_cadastro){
 
-     $this->id_paciente =  $id_paciente;
+     $this->id =  $id;
      $this->nome =  $nome;
-     $this->sobreNome = $sobreNome;
-     $this->dataDeNascimento = $dataDeNascimento;
-     $this->genero = $genero;
-     $this->dataDeCadastro =$dataDeCadastro;
+     $this->sobrenome = $sobrenome;
+     $this->data_nascimento = $data_nascimento;
+     $this->sexo = $sexo;
+     $this->data_cadastro = $data_cadastro;
     }
-     public function getId_paciente(){
-        return $this->id_paciente;
+     public function getId(){
+        return $this->id;
      }
-     public function setId_paciente(int $id_paciente){
-     $this->id_paciente = $id_paciente;
+     public function setId(int $id){
+     $this->id = $id;
      }
 
         public function getNome(){
@@ -30,32 +30,32 @@ class Paciente{
      $this->nome = $nome;
      }
 
-        public function getSobreNome(){
-        return $this->sobreNome;
+        public function getSobrenome(){
+        return $this->sobrenome;
      }
-     public function setSobreNome(string $sobreNome){
-     $this->sobreNome = $sobreNome;
-     }
-
-    public function getDataDeNascimento(){
-        return $this->dataDeNascimento;
-     }
-     public function setDataDeNascimento(string $dataDeNascimento){
-     $this->dataDeNascimento = $dataDeNascimento;
+     public function setSobrenome(string $sobrenome){
+     $this->sobrenome = $sobrenome;
      }
 
-        public function getGenero(){
-        return $this->genero;
+    public function getData_nascimento(){
+        return $this->data_nascimento;
      }
-     public function setGenero(string $genero){
-     $this->genero = $genero;
+     public function setData_nascimento(string $data_nascimento){
+     $this->data_nascimento = $data_nascimento;
      }
 
-    public function getDataDeCadastro(){
-        return $this->dataDeCadastro;
+        public function getSexo(){
+        return $this->sexo;
      }
-     public function setDataDeCadastro(DateTime $dataDeCadastro){
-     $this->dataDeCadastro = $dataDeCadastro;
+     public function setSexo(string $sexo){
+     $this->sexo = $sexo;
+     }
+
+    public function getData_cadastro(){
+        return $this->data_cadastro;
+     }
+     public function setData_cadastro(string $data_cadastro){
+     $this->data_cadastro = $data_cadastro;
      }
     }
 
--- gestao_hospitalar/index.php
+++ gestao_hospitalar/index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$isLogged = isset($_SESSION['token']);
 ?>
 
 <!DOCTYPE html>
@@ -6,20 +8,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
     <title>Gestão Hospitalar</title>
 </head>
 <body>
-    <h1>Gestão Hospitalar</h1>
-    <form action="" method="post">
-    <div>
-        <label for="email">Email :</label>
-        <input type="email" name="email" id="email" required>
-    </div>
-        <div>
-        <label for="senha">Senha :</label>
-        <input type="password" name="senha" id="senha" required>
-    </div>
-    <button type="submit">Logar</button>
-    </form>
+    <nav>
+        <a href="index.php">Home</a>
+        <?php if($isLogged):?>
+       <a href="./frontend/usuario.php">Minha Conta</a>
+        <a href="./frontend/logout.php">Sair</a>
+          <?php else:?>
+          <a href="./frontend/login.php">Login</a>
+        <a href="./frontend/cadastro.php">Cadastra-se</a>
+       <?php endif;?>
+    </nav>
+    
+    <h1>Bem vindo ao Sistema de Gestão Hospitalar</h1>
+    
+    <?php if($isLogged): ?>
+    <a href="./frontend/form_paciente.php">Cadastro de Paciente</a>
+     <?php endif; ?>
 </body>
 </html>
+      
